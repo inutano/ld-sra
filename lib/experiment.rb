8503d6bdc6838d68f8e9f4be5c79344a4e0b2e49
Tazro Inutano Ohta <inutano@gmail.com>
2020-01-30 17:36:34 +0900

    implement output_turtle, ready to test

--- lib/experiment.rb
+++ lib/experiment.rb
@@ -7,7 +7,7 @@ class SRAExperimentXML < Nokogiri::XML::SAX::Document
     @parent_name = []
     @inner_text = ""
 
-    @experiment_attr = {
+    @exp = {
       id: "",
       platform: "",
       instrument_model: "",
@@ -36,17 +36,17 @@ class SRAExperimentXML < Nokogiri::XML::SAX::Document
     case @parent_name.last
     when "LIBRARY_LAYOUT"
       h = attrs.to_h
-      @experiment_attr[:design][:library_layout][:type] = name
-      @experiment_attr[:design][:library_layout][:nominal_length] = h["NOMINAL_LENGTH"]
-      @experiment_attr[:design][:library_layout][:nominal_sdev] = h["NOMINAL_SDEV"]
+      @exp[:design][:library_layout][:type] = name.gsub("\s","_")
+      @exp[:design][:library_layout][:nominal_length] = h["NOMINAL_LENGTH"]
+      @exp[:design][:library_layout][:nominal_sdev] = h["NOMINAL_SDEV"]
     when "PLATFORM"
-      @experiment_attr[:platform] = name
+      @exp[:platform] = name.gsub("\s","_")
     end
     @parent_name << name
 
     case name
     when "EXPERIMENT"
-      @experiment_attr[:id] = attrs.to_h["accession"]
+      @exp[:id] = attrs.to_h["accession"]
     end
   end
 
@@ -57,21 +57,21 @@ class SRAExperimentXML < Nokogiri::XML::SAX::Document
   def end_element(name)
     case name
     when "TITLE"
-      @experiment_attr[:title] = @inner_text
+      @exp[:title] = @inner_text
     when "DESIGN_DESCRIPTION"
-      @experiment_attr[:design_description] = @inner_text
+      @exp[:design_description] = @inner_text
     when "LIBRARY_NAME"
-      @experiment_attr[:design][:library_name] = @inner_text
+      @exp[:design][:library_name] = @inner_text
     when "LIBRARY_STRATEGY"
-      @experiment_attr[:design][:library_strategy] = @inner_text
+      @exp[:design][:library_strategy] = @inner_text.gsub("\s","_")
     when "LIBRARY_SOURCE"
-      @experiment_attr[:design][:library_source] = @inner_text
+      @exp[:design][:library_source] = @inner_text.gsub("\s","_")
     when "LIBRARY_SELECTION"
-      @experiment_attr[:design][:library_selection] = @inner_text
+      @exp[:design][:library_selection] = @inner_text.gsub("\s","_")
     when "LIBRARY_CONSTRUCTION_PROTOCOL"
-      @experiment_attr[:design][:library_construction_protocol] = @inner_text
+      @exp[:design][:library_construction_protocol] = @inner_text
     when "INSTRUMENT_MODEL"
-      @experiment_attr[:instrument_model] = @inner_text
+      @exp[:instrument_model] = @inner_text.gsub("\s","_")
     when "EXPERIMENT"
       output_turtle
     end
@@ -82,7 +82,7 @@ class SRAExperimentXML < Nokogiri::XML::SAX::Document
   #
 
   def write_prefixes
-    puts "@base <http://bio.cow/ontology/sra-experiement/> ."
+    puts "@prefix : <http://bio.cow/ontology/sra-experiement/> ."
     puts "@prefix id: <http://identifiers.org/insdc.sra/> ."
     puts "@prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> ."
     puts "@prefix dct: <http://purl.obolibrary.org/obo/> ."
@@ -90,48 +90,28 @@ class SRAExperimentXML < Nokogiri::XML::SAX::Document
   end
 
   def output_turtle
-    p @experiment_attr
+    puts "id:#{@exp[:id]} a :Experiment ;"
+    puts "  rdfs:label \"#{@exp[:id]}: #{@exp[:title]}\" ;"
+    puts "  dct:identifier \"#{@exp[:id]}\" ;"
+    puts "  :designDesctiption \"#{@exp[:design_description]}\" ;"
+    puts "  dct:description \"#{@exp[:design_description]}\" ;"
+    puts "  :title \"#{@exp[:title]}\" ;"
+    puts "  :platform ["
+    puts "    a :#{@exp[:platform]} ;"
+    puts "    :instrumentModel :#{@exp[:instrument_model]}"
+    puts "  ] ;"
+    puts "  :design ["
+    puts "    a :ExperimentDesign ;"
+    puts "    :libraryName \"#{@exp[:design][:library_name]}\" ;"
+    puts "    :libraryStrategy :#{@exp[:design][:library_strategy]} ;"
+    puts "    :librarySource :#{@exp[:design][:library_source]} ;"
+    puts "    :librarySelection :#{@exp[:design][:library_selection]} ;"
+    puts "    :libraryConstructionProtocol \"#{@exp[:design][:library_construction_protocol]}\" ;"
+    puts "    :libraryLayout ["
+    puts "      a :#{@exp[:design][:library_layout][:type]} ;"
+    puts "      :nominalLength \"#{@exp[:design][:library_layout][:nominal_length]}\" ;"
+    puts "      :nominalSdev \"#{@exp[:design][:library_layout][:nominal_sdev]}\""
+    puts "    ]"
+    puts "  ] ."
   end
-
-  # def attribute(attrs)
-  #   h = attrs.to_h
-  #   @sample_properties[:additional_properties] << {
-  #     attribute_name: h["attribute_name"],
-  #     harmonized_name: h["harmonized_name"],
-  #     display_name: h["display_name"],
-  #   }
-  # end
-  #
-  # def attribute_value
-  #   h = @sample_properties[:additional_properties].pop
-  #   h[:property_value] = @inner_text
-  #   @sample_properties[:additional_properties] << h
-  # end
-  #
-  # def output_turtle
-  #   puts "bs:#{@sample_properties[:id]} a <DataRecord>;"
-  #   puts "  <dateCreated> \"#{@sample_properties[:submission_date]}\"^^<Date>;"
-  #   puts "  <dateModified> \"#{@sample_properties[:last_update]}\"^^<Date>;"
-  #   puts "  <identifier> \"biosample:#{@sample_properties[:id]}\";"
-  #   puts "  <isPartOf> <https://www.ebi.ac.uk/biosamples/samples>;"
-  #   puts "  <mainEntity> ["
-  #   puts "    a <Sample>, obo:OBI_0000747;"
-  #   puts "    <name> \"#{@sample_properties[:id]}\";"
-  #   puts "    <identifier> \"biosample:#{@sample_properties[:id]}\";"
-  #   puts "    dct:identifier \"#{@sample_properties[:id]}\";"
-  #   puts "    <subjectOf> \"https://www.ebi.ac.uk/ena/data/view/#{@sample_properties[:id]}\";"
-  #   puts "    <description> \"#{@sample_properties[:description_title]}\";"
-  #
-  #   puts "    <additionalProperty> ["
-  #   n = @sample_properties[:additional_properties].size
-  #   @sample_properties[:additional_properties].each_with_index do |p,i|
-  #     puts "      a <PropertyValue>;"
-  #     puts "      <name> \"#{p[:harmonized_name] ? p[:harmonized_name] : p[:attribute_name]}\";"
-  #     puts "      <value> \"#{p[:property_value]}\""
-  #     if i != n-1
-  #       puts "    ], ["
-  #     end
-  #   end
-  #   puts "    ] ."
-  # end
 end
