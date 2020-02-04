ba7de5de96d9aed94febdf2f4727314e4a08a915
Tazro Inutano Ohta <inutano@gmail.com>
2020-02-04 17:53:44 +0900

    paths are hardcodeed, doesnt work on other machines anyway

--- sh/exp.job.sh
+++ sh/exp.job.sh
@@ -3,7 +3,7 @@
 set -eux
 
 # Load rbenv
-export PATH="${HOME}/.rbenv/bin:${PATH}"
+export PATH="/home/inutano/.rbenv/bin:${PATH}"
 eval "$(rbenv init -)"
 
 # ARGS
@@ -11,13 +11,13 @@ EXP_XML_PATH=$(awk -v id=${SGE_TASK_ID} 'NR == id { print $0 }' ${1})
 ACC=$(basename ${EXP_XML_PATH} .experiment.xml)
 
 # Path to directories
-SH_DIR=$(cd $(dirname ${0}) && pwd -P)
-DEST_DIR="${SH_DIR}/../data/${ACC:0:6}/${ACC}"
+BASE_DIR="/home/inutano/repos/ld-sra"
+DEST_DIR="${BASE_DIR}/data/${ACC:0:6}/${ACC}"
 ttl_path="${DEST_DIR}/${ACC}.experiment.ttl"
 
 # run xml2ttl
 if [[ ! -e "${ttl_path}" ]]; then
-  mkdir -p ${DEST_DIR}
-  cd ${SH_DIR}/.. && ./xml2ttl experiment ${EXP_XML_PATH} |\
+  mkdir -p "${DEST_DIR}"
+  ${BASE_DIR}/xml2ttl experiment ${EXP_XML_PATH} |\
     | grep -v "^@prefix" > "${ttl_path}"
 fi
--- sh/exp.run.sh
+++ sh/exp.run.sh
@@ -5,7 +5,7 @@ set -eux
 FASTQ_DIR="/usr/local/resources/dra/fastq"
 
 # The path to the directories
-BASE_DIR="$(cd $(dirname ${0}) && pwd -P)/.."
+BASE_DIR="/home/inutano/repos/ld-sra"
 RESULT_DIR="${BASE_DIR}/data"
 WORK_DIR="${RESULT_DIR}/$(date "+%Y%m%d")"
 mkdir -p "${WORK_DIR}"
@@ -13,13 +13,13 @@ mkdir -p "${WORK_DIR}"
 # Create array job configuration
 cd ${WORK_DIR} && \
   find ${FASTQ_DIR} -name '*.experiment.xml' |\
-  split -l 5000 -d - "exp.array."
+  split -l 5000 -d - "exp."
 
 # Load UGE settings
 source "/home/geadmin/UGED/uged/common/settings.sh"
 
 # Execute array job
-find ${WORK_DIR} -name "exp.array.*" | sort | while read jobconf; do
+find ${WORK_DIR} -name "exp.*" | sort | while read jobconf; do
   jobname=$(basename ${jobconf})
   qsub -j y -N "${jobname}" -o /dev/null -pe def_slot 1 -l s_vmem=4G -l mem_req=4G -t 1-5000:1 \
     ${BASE_DIR}/sh/exp.job.sh ${jobconf}
