eb54eabb153e905b82f822d63f5f426a594a277b
Tazro Inutano Ohta <inutano@gmail.com>
2020-10-15 17:05:14 +0900

    Update run/job scripts, remove Dockerfile

--- sh/exp.job.sh
+++ sh/exp.job.sh
@@ -1,49 +1,60 @@
 #!/bin/sh
 #$ -S /bin/sh -j y
 # set -x
+# For DDBJ supercomputer system
+#  ./exp.job.sh <path to job configuration> <output dir>
+
+#
+# Load Docker configuration
+#
 module load docker
 
-# Load rbenv
-export PATH="/home/inutano/.rbenv/bin:${PATH}"
-eval "$(rbenv init -)"
-
-# ARGS
-EXP_XML_PATH=$(awk -v id=${SGE_TASK_ID} 'NR == id { print $0 }' ${1})
-ACC=$(basename ${EXP_XML_PATH} .experiment.xml)
-
-# Path to directories
-OUTDIR="${2}/${ACC:0:6}/${ACC}"
-mkdir -p "${OUTDIR}"
-
-TMPDIR="/data1/tmp/biosample-lod/experiment/ttl"
-tmp_ttl_path="${TMPDIR}/${ACC:0:6}/${ACC}/${ACC}.experiment.ttl"
-
-# run xml2ttl
-mkdir -p $(dirname ${tmp_ttl_path})
-docker run --security-opt seccomp=unconfined --rm \
-  -v $(dirname ${EXP_XML_PATH}):/work \
-  -w /work \
-  "quay.io/inutano/ld-sra:v1.4" \
-  xml2ttl \
-  experiment \
-  $(basename ${EXP_XML_PATH}) > "${tmp_ttl_path}"
+#
+# Constants
+#
+JOBCONF_PATH="${1}"
+OUTDIR="${2}"
+OUT_TTL_PATH="${OUTDIR}/$(basename ${1}).ttl"
+SCRIPT_PATH="$(cd $(dirname ${0}) && pwd -P)/../python/script/expxml2ttl.py"
+DOCKER_IMAGE_TAG="python:3.9.0-buster"
 
 #
-# Validate ttl
+# Functions
 #
-validation_output="${tmp_ttl_path}.validation"
-valid_value='Validator finished with 0 warnings and 0 errors.'
+xml2ttl() {
+  docker run --security-opt seccomp=unconfined --rm -i \
+    -v ${SCRIPT_PATH}:/$(basename ${SCRIPT_PATH}) \
+    -v ${JOBCONF_PATH}:/$(basename ${JOBCONF_PATH}) \
+    -v /usr/local/resources/dra/fastq:/usr/local/resources/dra/fastq \
+    ${DOCKER_IMAGE_TAG} \
+    python \
+    "/$(basename ${SCRIPT_PATH})" \
+    -l "/${JOBCONF_PATH}" \
+    > "${OUT_TTL_PATH}"
+}
 
-docker run --security-opt seccomp=unconfined --rm \
-  -v $(dirname "${tmp_ttl_path}"):/work \
-  "quay.io/inutano/turtle-validator:v1.0" \
-  ttl \
-  /work/$(basename "${tmp_ttl_path}") \
-  > "${validation_output}"
+validate_ttl() {
+  local validation_output="${OUT_TTL_PATH}.validation"
+  local valid_value='Validator finished with 0 warnings and 0 errors.'
 
-if [[ $(cat "${validation_output}") == "${valid_value}" ]]; then
-  rm -f "${validation_output}"
-  cat ${tmp_ttl_path} | grep -v "^@prefix" > ${OUTDIR}/$(basename ${tmp_ttl_path})
-else
-  mv "${validation_output}" ${OUTDIR}
-fi
+  docker run --security-opt seccomp=unconfined --rm -i \
+    -v $(dirname "${OUT_TTL_PATH}"):/work \
+    "quay.io/inutano/turtle-validator:v1.0" \
+    ttl \
+    /work/$(basename "${OUT_TTL_PATH}") \
+    > "${validation_output}"
+
+  if [[ $(cat "${validation_output}") == "${valid_value}" ]]; then
+    rm -f "${validation_output}"
+  fi
+}
+
+run() {
+  xml2ttl
+  validate_ttl
+}
+
+#
+# Exec
+#
+run
--- sh/exp.run.sh
+++ sh/exp.run.sh
@@ -1,66 +1,68 @@
 #!/bin/bash
 # set -eux
+# For DDBJ supercomputer system
 
-# Path to the directory DRA storage mounted
-[[ -z ${FASTQ_DIR} ]] && FASTQ_DIR="/usr/local/resources/dra/fastq"
-printf "Use ${FASTQ_DIR} for XML source..\n"
-
-# Path to job script
-JOB_SCRIPT_PATH="$(cd $(dirname $0) && pwd -P)/exp.job.sh"
-
-# Path to working directories
-if [[ -z ${1} ]]; then
-  WORKDIR="$(cd $(dirname $0) && pwd -P)/../data/$(date "+%Y%m%d")"
-else
-  mkdir -p "${1}"
-  WORKDIR="$(cd ${1} && pwd -P)"
-fi
-
-JOBCONF_DIR="${WORKDIR}/jobconf"; mkdir -p "${JOBCONF_DIR}"
-TTL_DIR="${WORKDIR}/ttl";         mkdir -p "${TTL_DIR}"
-# UGE_LOG_DIR="${WORKDIR}/log";     mkdir -p "${UGE_LOG_DIR}" # For debug
-
-ttl_prefixes() {
-  cat <<EOS
-@prefix : <http://bio.cow/ontology/sra-experiement/> .
-@prefix id: <http://identifiers.org/insdc.sra/> .
-@prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
-@prefix dct: <http://purl.obolibrary.org/obo/> .
-
-EOS
-}
-
-# Create array job configuration
-if [[ -z $(find ${JOBCONF_DIR} -name 'exp.*') ]]; then
-  cd ${JOBCONF_DIR}
-  find ${FASTQ_DIR} -type f -name '*.experiment.xml' | split -l 5000 -d - "exp."
-fi
-
-# Load UGE settings
+#
+# Load UGE configuration
+#
 source "/home/geadmin/UGED/uged/common/settings.sh"
 
-# Execute array job
-find ${JOBCONF_DIR} -name "exp.*" | sort | while read jobconf; do
-  qsub -N "$(basename ${jobconf})" \
-    -j y \
-    -o /dev/null \
-    -pe def_slot 1 -l s_vmem=4G -l mem_req=4G \
-    -t 1-$(wc -l "${jobconf}" | awk '$0=$1'):1 \
-    ${JOB_SCRIPT_PATH} ${jobconf} ${TTL_DIR}
-done
+#
+# Constants
+#
+FASTQ_DIR="/usr/local/resources/dra/fastq"
+JOB_SCRIPT_PATH="$(cd $(dirname $0) && pwd -P)/exp.job.sh"
+WORKDIR="$(cd $(dirname $0) && pwd -P)/../data/$(date "+%Y%m%d")"
+JOBCONF_DIR="${WORKDIR}/jobconf"
+TTL_DIR="${WORKDIR}/ttl"
+UGE_LOG_DIR="${WORKDIR}/log"
 
-# Wait until the all jobs complete
-while :; do
-  sleep 30
-  running_jobs=$(qstat | grep "exp.")
-  if [[ -z ${running_jobs} ]]; then
-    printf "All jobs finished.\n"
-    break
+#
+# Functions
+#
+setup_dirs() {
+  mkdir -p "${WORKDIR}"
+  mkdir -p "${JOBCONF_DIR}"
+  mkdir -p "${TTL_DIR}"
+  mkdir -p "${UGE_LOG_DIR}"
+}
+
+find_experiment_xml() {
+  if [[ -z $(find ${JOBCONF_DIR} -name 'exp.*') ]]; then
+    cd ${JOBCONF_DIR}
+    find ${FASTQ_DIR} -type f -name '*.experiment.xml' | split -l 50000 -d - "exp."
   fi
-done
+}
 
-# Assemble the ttl files by the accession number group
-find "${TTL_DIR}" -maxdepth 1 -name '*RA*' -type d | while read dir; do
-  cat <(ttl_prefixes) <(find ${dir} -name '*ttl' | xargs cat) > "${TTL_DIR}/$(basename ${dir}).ttl"
-  # rm -fr ${dir}
-done
+submit_arrayjob() {
+  find ${JOBCONF_DIR} -name "exp.*" | sort | while read jobconf; do
+    qsub -N "$(basename ${jobconf})" \
+      -j y \
+      -o "${UGE_LOG_DIR}/$(basename ${jobconf}).log" \
+      -pe def_slot 1 -l s_vmem=4G -l mem_req=4G \
+      ${JOB_SCRIPT_PATH} ${jobconf} ${TTL_DIR}
+  done
+}
+
+wait_uge_jobs() {
+  while :; do
+    sleep 30
+    running_jobs=$(qstat | grep "exp.")
+    if [[ -z ${running_jobs} ]]; then
+      printf "All jobs finished.\n"
+      break
+    fi
+  done
+}
+
+run() {
+  setup_dirs
+  find_experiment_xml
+  submit_arrayjob
+  wait_uge_jobs
+}
+
+#
+# Exec
+#
+run
